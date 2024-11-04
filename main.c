f09cc4e8df23e552e7516fc564e9a49a31b8abe7
Henrique Pereira <116195064+Hhpp2004@users.noreply.github.com>
2024-11-03 22:42:56 -0300

    feat. algoritmo de entrada pronto

--- main.c
+++ main.c
@@ -2,5 +2,18 @@
 
 int main()
 {
+    int num,deletar,ordem;
+    arvore_b *raiz = NULL;
+    scanf("%i", &ordem);
+    raiz = cria_arvore(ordem);
+    do
+    {
+        scanf("%i",&num);
+        if(num != -1)
+        {
+            insere_arvore_b(raiz,num);
+        }
+    } while (num != -1);
+    
     return 0;
 }
